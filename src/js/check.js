8398489d56792e55b59379f8f3c12a91c5c6fb5f
Elena Egorova <luuuv@yandex.ru>
2016-10-07 19:13:41 +0300

    Kekstagram v1 - bugfix1

--- src/js/check.js
+++ src/js/check.js
@@ -1,31 +1,36 @@
 "use strict";
 var getMessage = function (a, b) {
 	
-	if (typeof a == "boolean") {
-		if (a == true) {
-			return ("Переданное GIF-изображение анимировано и содержит " + b + " кадров");
+	if (typeof a === "boolean") {
+		if (a === true) {
+			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
 		}
 		else {
-			return ("Переданное GIF-изображение не анимировано");
+			return "Переданное GIF-изображение не анимировано";
 		}
-	} else if (typeof a == "number") {
-		return ("Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов");
-	} else if (typeof a == "object" && typeof b != "object") {
+	} else if (typeof a === "number") {
+		return "Переданное SVG-изображение содержит " + a + " объектов и " + b*4 + " атрибутов";
+		
+		
+	} else if (Array.isArray(a) === true && Array.isArray(b) !== true) {
 		var amountOfRedPoints = 0;
 		for (var i = 0; i < a.length; i++) {
 			amountOfRedPoints += a[i];
 		}
-		return ( "Количество красных точек во всех строчках изображения: " + amountOfRedPoints);
-	} else if (typeof a == "object" && typeof b == "object") {
+
+		return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+		
+		
+	} else if (Array.isArray(a) === true && Array.isArray(b) === true) {
 		var artifactsSquare = 0;
-		if (a.lenght == b.lenght) {
-			for (var j = 0; j < a.length; j++) {
-				var c = a[j] * b[j];
-				artifactsSquare += c;
-			}
+		for (var j = 0; j < a.length; j++) {
+			var c = a[j] * b[j];
+			artifactsSquare += c;
 		}		
-		return ("Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей");
+		return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+		
+		
 	} else {
-		return ("Переданы некорректные данные");
+		return "Переданы некорректные данные";
 	}
 }
