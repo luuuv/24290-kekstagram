26a7877b79a0e78268e7edf2783c4a991548ebc4
Elena Egorova <luuuv@yandex.ru>
2016-10-17 18:59:16 +0300

    Validation v2

--- src/js/upload.js
+++ src/js/upload.js
@@ -77,25 +77,22 @@
   var inputLength = document.getElementById('resize-size');
 
   var resizeFormIsValid = function() {
-    if (Number(inputLeft.value) + Number(inputLength.value) > currentResizer._image.naturalWidth) {
-      document.getElementById('resize-fwd').disabled = true;
-      return false;
-    } else if (Number(inputTop.value) + Number(inputLength.value) > currentResizer._image.naturalHeight) {
-      document.getElementById('resize-fwd').disabled = true;
-      return false;
-    } else if (Number(inputTop.value) < 0 || Number(inputLeft.value) < 0) {
-      document.getElementById('resize-fwd').disabled = true;
+    var inputLeftValue = Number(inputLeft.value);
+    var inputTopValue = Number(inputTop.value);
+    var inputLengthValue = Number(inputLength.value);
+    var resizeSubmitState = document.getElementById('resize-fwd');
+    console.log(typeof document.getElementById('resize-fwd').disabled);
+    if ((inputLeftValue + inputLengthValue > currentResizer._image.naturalWidth) || (inputTopValue + inputLengthValue > currentResizer._image.naturalHeight) || (inputTopValue < 0 || inputLeftValue < 0)) {
+      resizeSubmitState.disabled = true;
       return false;
     } else {
-      document.getElementById('resize-fwd').disabled = false;
+      resizeSubmitState.disabled = false;
       return true;
     }
   };
 
   inputLeft.oninput = function() {
     resizeFormIsValid();
-    //console.log(inputLeft.value);
-    //document.getElementById('resize-fwd').disabled = true;
   };
   inputTop.oninput = function() {
     resizeFormIsValid();
